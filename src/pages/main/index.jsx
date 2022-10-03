6c4a01d0b9aa18e508c8540da8628cd14d6a704c
enesshala <enesishala@gmail.com>
2022-10-03 21:30:46 +0200

    text changed

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const index = () => {
   return (
     <div>
-      <h1 className="text-2xl text-purple-800">Welcome to Home Page</h1>
+      <h1 className="text-2xl text-purple-800">
+        Welcome to Home Page of PICA APP
+      </h1>
     </div>
   );
 };
